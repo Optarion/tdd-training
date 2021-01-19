abf3b5850d5f272a4aa0f49fab3716ea035f137c
Adrien Rousselet <adrien.rousselet@thetribe.io>
2021-01-19 09:41:32 +0100

    wip refactor

--- katas/romanNumerals/romanNumerals.js
+++ katas/romanNumerals/romanNumerals.js
@@ -1,16 +1,21 @@
 export default number => {
-    if (number < 4) return 'I'.repeat(number);
-    if (number === 4) return 'IV';
-    if (number === 5) return 'V';
-    if (number === 9) return 'IX';
-    if (number < 10) return `V${'I'.repeat(number-5)}`;
-    if (number === 10) return 'X';
-    if (number === 11) return 'XI';
-    if (number === 12) return 'XII';
-    if (number === 15) return 'XV';
-   
-    if (number === 1000) return 'M';
+    let rest = number;
+    let result = '';
 
+    while (rest) {
+        if (number < 4) return 'I'.repeat(number);
+        if (number === 4) return 'IV';
+        if (number === 5) return 'V';
+        if (number === 9) return 'IX';
+        if (number < 10) return `V${'I'.repeat(number-5)}`;
+        if (number === 10) return 'X';
+        if (number === 11) return 'XI';
+        if (number === 12) return 'XII';
+        if (number === 15) return 'XV';
+        if (number === 1000) return 'M';
+    }
+   
+    return result;
 };
 
 // Difficulté à faire un refacto incrémental. L'algo n'est pas encore clair pour moi donc 
